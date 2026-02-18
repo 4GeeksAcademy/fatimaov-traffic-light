ae3d150166eb322696ae07c414e2fa246c633c68
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-18 21:25:41 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TrafficLight from "./TrafficLight";
+
+// Styles
+const container = {
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+}
+
+const containerTop = {
+	display: "block",
+	width: "20px",
+	height: "150px",
+	backgroundColor: "black",
+}
+
+const containerBottom = {
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	gap: "10px",
+	padding: "20px",
+	borderRadius: "10px",
+	backgroundColor: "black",
+}
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<div style={container}>
+			<div style={containerTop}></div>
+			<div style={containerBottom}>
+				<TrafficLight />
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
 	);
 };
---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+// Styles
+const light = {
+    display: "block",
+    width: "150px",
+    height: "150px",
+    borderRadius: "100%",
+    backgroundColor: "black",
+}
+
+function TrafficLight() {
+    const [redBoxShadow, setRedBoxShadow] = useState(null);
+    const [yellowBoxShadow, setYellowBoxShadow] = useState(null);
+    const [greenBoxShadow, setGreenBoxShadow] = useState(null);
+
+    const isOn = { boxShadow: "0px 0px 20px 10px white" };
+    const isOff = null;
+
+    function handleClickRed() {
+        if (!redBoxShadow) {
+            setRedBoxShadow(isOn)
+            setYellowBoxShadow(isOff)
+            setGreenBoxShadow(isOff)
+        }
+    }
+
+    function handleClickYellow() {
+        if (!yellowBoxShadow) {
+            setYellowBoxShadow(isOn)
+            setRedBoxShadow(isOff)
+            setGreenBoxShadow(isOff)
+        }
+    }
+
+    function handleClickGreen() {
+        if (!greenBoxShadow) {
+            setGreenBoxShadow(isOn)
+            setRedBoxShadow(isOff)
+            setYellowBoxShadow(isOff)
+        }
+    }
+
+    return (
+        <>
+            <div style={{ ...light, ...{ backgroundColor: "red" }, ...redBoxShadow }} onClick={handleClickRed}></div >
+            <div style={{ ...light, ...{ backgroundColor: "yellow" }, ...yellowBoxShadow }} onClick={handleClickYellow}></div>
+            <div style={{ ...light, ...{ backgroundColor: "green" }, ...greenBoxShadow }} onClick={handleClickGreen}></div>
+        </>
+    )
+}
+
+export default TrafficLight;
